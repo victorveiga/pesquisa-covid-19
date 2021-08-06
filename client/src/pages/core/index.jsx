b3bf5d5704c879949ebfe82400da68c3d35ee9e4
Victor Veiga <44455077+victorveiga@users.noreply.github.com>
2021-08-05 21:19:58 -0300

    fix the main page

--- client/src/pages/core/index.jsx
+++ client/src/pages/core/index.jsx
@@ -1,24 +1,38 @@
 import React, {Component} from 'react';
-import {Form} from 'react-bootstrap';
+import {Form, Button} from 'react-bootstrap';
+import './index.css';
 
 
 export default class Core extends Component {
     render(){
         return <>
-            <div>
-                <Form.Label>Você precisou trocar o perfil de usuário da internet, durante o confinamento?</Form.Label>
-                <Form.Check inline name="q1" type="radio" label="Sim" />
-                <Form.Check inline name="q1" type="radio" label="Não" />
+            <div style={{padding: 10}}>
+                <img id="logo_" src="https://www.unifeso.edu.br/images/logo/UNIFESO.png" width="220px" />
             </div>
-            <div>
-                <Form.Label>Caso tenha respondido “Sim” na pergunta anterior, indique o motivo? (você pode responder mais de uma):</Form.Label>
-            </div>
-            <Form.Check type="checkbox" label="1 - Devido ao número de pessoas precisando utilizar a internet em casa. " />
-            <Form.Check type="checkbox" label="2 - Devido a necessidade de lazer (assistir filmes, games, Tv etc.)" />
-            <Form.Check type="checkbox" label="3 - Devido ao serviço de home office." />
-            <Form.Check type="checkbox" label="4 - Devido ao serviço de home office e utilização do Ambiente Virtual de Aprendizagem (AVA)." />
-            <Form.Check type="checkbox" label="5 - Devido a utilização do Ambiente Virtual de Aprendizagem (AVA)." />
-            <Form.Check type="checkbox" label="6 - Outra" />            
+            <div className="d-flex justify-content-center mt-3 p-4" >
+                <div>
+                    <div className="d-flex justify-content-end m-4">
+                        <Button variant="outline-danger" className="mr-2">Voltar</Button>
+                        <Button variant="outline-success">Avançar</Button>
+                    </div>
+                    <div>
+                        <Form.Label>Você precisou trocar o perfil de usuário da internet, durante o confinamento?</Form.Label>
+                        <div>
+                            <Form.Check inline name="q1" type="radio" label="Sim" />
+                            <Form.Check inline name="q1" type="radio" label="Não" />
+                        </div>
+                    </div>
+                    <div>
+                        <Form.Label>Caso tenha respondido “Sim” na pergunta anterior, indique o motivo? (você pode responder mais de uma):</Form.Label>
+                    </div>
+                    <Form.Check type="checkbox" label="1 - Devido ao número de pessoas precisando utilizar a internet em casa. " />
+                    <Form.Check type="checkbox" label="2 - Devido a necessidade de lazer (assistir filmes, games, Tv etc.)" />
+                    <Form.Check type="checkbox" label="3 - Devido ao serviço de home office." />
+                    <Form.Check type="checkbox" label="4 - Devido ao serviço de home office e utilização do Ambiente Virtual de Aprendizagem (AVA)." />
+                    <Form.Check type="checkbox" label="5 - Devido a utilização do Ambiente Virtual de Aprendizagem (AVA)." />
+                    <Form.Check type="checkbox" label="6 - Outra" /> 
+                </div>
+            </div>         
         </>
     }
 }
