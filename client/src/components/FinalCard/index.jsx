bf4c15324da5fab1914e6d61bdea928fa04c7b7f
Victor Veiga <44455077+victorveiga@users.noreply.github.com>
2021-09-02 11:01:41 -0300

    Update index.jsx

--- client/src/components/FinalCard/index.jsx
+++ client/src/components/FinalCard/index.jsx
@@ -10,9 +10,31 @@ export default class FinalCard extends Component {
         this.save()
     }
 
+    getCookie(name) {
+        let cookieValue = null;
+        if (document.cookie && document.cookie !== '') {
+            const cookies = document.cookie.split(';');
+            for (let i = 0; i < cookies.length; i++) {
+                const cookie = cookies[i].trim();
+                // Does this cookie string begin with the name we want?
+                if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
+
     save(){
         if (this.props.form.state.readOnly === true) return null;
-        api.post('participante/', this.props.form.state)
+        const csrftoken = this.getCookie('csrftoken');
+        api.post('participante/', this.props.form.state, {
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRFToken': csrftoken
+            }
+        })
             .then(response => {
                 console.log('Pesquisa submetida com sucesso!', response);
             })
