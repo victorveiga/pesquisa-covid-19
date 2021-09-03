cfc9680fe56c4960f283cea223b8f8d2a81ee6f2
Victor Veiga <44455077+victorveiga@users.noreply.github.com>
2021-09-02 22:00:43 -0300

    Adding download csv data action

--- server/participante/admin.py
+++ server/participante/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
 from .models import Dados
 
+def handleSpecificDataCSV(modeladmin, request, queryset):
+    queryset.update(bairro='teste')
+
+def handleAllDataCSV(modeladmin, request, queryset):
+    queryset.update(bairro='teste')
+
+handleSpecificDataCSV.short_description = "Baixar registros marcados em CSV"
+handleAllDataCSV.short_description = "Baixar todos os registros"
+
+class DadosAdmin(admin.ModelAdmin):
+    actions = [handleSpecificDataCSV, handleAllDataCSV]
+
+    def get_actions(self, request):
+        actions = super().get_actions(request)
+        if 'delete_selected' in actions:
+            del actions['delete_selected']
+        return actions
+
 # Register your models here.
-admin.site.register(Dados)
+admin.site.register(Dados, DadosAdmin)
