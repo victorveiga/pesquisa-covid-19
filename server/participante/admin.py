ac3d6780749a65b6a794a7f3c8061bf200e59030
Victor Veiga <44455077+victorveiga@users.noreply.github.com>
2021-09-03 09:03:26 -0300

    Added download csv data action

--- server/participante/admin.py
+++ server/participante/admin.py
@@ -1,14 +1,30 @@
 from django.contrib import admin
 from .models import Dados
+from django.http import HttpResponse
+import csv
+
+def download_csv(model, queryset):
+    meta = model.model._meta
+    field_names = [field.name for field in meta.fields]
+
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
+    writer = csv.writer(response)
+
+    writer.writerow(field_names)
+    for obj in queryset:
+        writer.writerow([getattr(obj, field) for field in field_names])
+
+    return response
 
 def handleSpecificDataCSV(modeladmin, request, queryset):
-    queryset.update(bairro='teste')
+    return download_csv(modeladmin, queryset)
 
 def handleAllDataCSV(modeladmin, request, queryset):
-    queryset.update(bairro='teste')
+    return download_csv(modeladmin, Dados.objects.all())
 
-handleSpecificDataCSV.short_description = "Baixar registros marcados em CSV"
-handleAllDataCSV.short_description = "Baixar todos os registros"
+handleSpecificDataCSV.short_description = "Baixar em .CSV registros marcados"
+handleAllDataCSV.short_description = "Baixar em .CSV todos os registros"
 
 class DadosAdmin(admin.ModelAdmin):
     actions = [handleSpecificDataCSV, handleAllDataCSV]
