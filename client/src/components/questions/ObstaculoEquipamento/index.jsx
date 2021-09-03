20588cbef7aefa34cb1a14ebf14e8866bfea46c2
Victor Veiga <44455077+victorveiga@users.noreply.github.com>
2021-09-02 22:15:11 -0300

    Update index.jsx

--- client/src/components/questions/ObstaculoEquipamento/index.jsx
+++ client/src/components/questions/ObstaculoEquipamento/index.jsx
@@ -32,7 +32,7 @@ export default class ObstaculoEquipamento extends Component {
                 form={this.props.form}
                 label="Especificar outros obtáculos do motivo da falta de equipamentos"
                 placeholder="Escreva aqui"
-                field="obstaculo_equip_espec"
+                field="obstaculo_equipamento_espec"
                 required={this.props.form.state.obstaculo_equipamento && this.props.form.state.obstaculo_equipamento === "4" ? true : false}
                 feedback_invalid="Por favor, especifique os obtáculos"
             />}
